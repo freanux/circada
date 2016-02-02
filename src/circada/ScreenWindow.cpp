0a5ccdf468c4ade0f74c499a2036a1f04e31972f
freanux <pw@earthwave.ch>
2016-02-02 13:53:02 +0100

    sort fixed

--- src/circada/ScreenWindow.cpp
+++ src/circada/ScreenWindow.cpp
@@ -79,17 +79,16 @@ int ScreenWindow::get_sequence() {
 }
 
 bool ScreenWindowComparer::operator()(ScreenWindow* const& lhs, ScreenWindow* const& rhs) {
-    if (lhs == rhs) {
+    if (lhs->get_circada_session() == rhs->get_circada_session()) {
         Circada::Window *lhs_w = lhs->get_circada_window();
         Circada::Window *rhs_w = rhs->get_circada_window();
-
         if (lhs_w->get_window_type() == rhs_w->get_window_type()) {
             return lhs_w->get_name() < rhs_w->get_name();
         }
         return lhs_w->get_window_type() < rhs_w->get_window_type();
     }
 
-    return lhs < rhs;
+    return lhs->get_circada_session() < rhs->get_circada_session();
 }
 
 void ScreenWindow::cleanup() {
